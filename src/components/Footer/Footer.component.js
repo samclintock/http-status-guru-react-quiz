818ec52fb421c3485adfaaeadcb1f795400b6c7e
Steven McLintock <stevenmclintock@fastmail.com>
2019-03-15 10:32:56 -0400

    Update footer content

--- src/components/Footer/Footer.component.js
+++ src/components/Footer/Footer.component.js
@@ -5,7 +5,7 @@ class Footer extends React.Component {
     render() {
         return <footer class="page-footer font-small">
             <div class="text-center py-3">
-                Footer
+                Built using <a href="https://reactjs.org/">React</a> by <a href="https://kiltandcode.com/">Kilt &amp; Code</a>
             </div>
         </footer>
     }
